7a7126a8c28d6de95aa214be167c022e3bf513e2
kaoutaar <51215027+kaoutaar@users.noreply.github.com>
2025-04-12 14:34:20 +0200

    Update utils.py v3 - remove path type restriction in create speaker

--- outetts/version/v3/utils.py
+++ outetts/version/v3/utils.py
@@ -7,8 +7,6 @@ def create_speaker(
         whisper_model: str = "turbo",
         whisper_device = None,
     ):
-    if not os.path.exists(audio_path):
-        raise FileNotFoundError(f"Audio file not found: {audio_path}")
 
     return audio_processor.create_speaker_from_whisper(
         audio=audio_path,
